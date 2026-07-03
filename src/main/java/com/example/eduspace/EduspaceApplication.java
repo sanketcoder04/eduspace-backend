e6a26bbeadaa59f778382cf4c2a87f82bf8ffdf3
Sanket Ganguly <sanketganguly53@gmail.com>
2026-07-04 01:34:47 +0530

    Project Setup

---
+++ src/main/java/com/example/eduspace/EduspaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eduspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduspaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eduspace/EduspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eduspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
